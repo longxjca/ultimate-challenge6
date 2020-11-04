456536ef545417cb9e22aa1697def42156d1056f
Longxun Jin <longxjca@gmail.com>
2020-11-04 16:10:24 -0600

    added a image

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -5,6 +5,15 @@ ActiveAdmin.register Customer do
   # Uncomment all parameters which should be permitted for assignment
   #
   permit_params :name, :phone, :email, :notes, :image
+  # Formtastic!
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions         # adds the 'Submit' and 'Cancel' buttons
+  end
   #
   # or
   #
