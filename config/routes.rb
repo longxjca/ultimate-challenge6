a07485bf4d03db77122b6c9f764b4a304677ec7e
Longxun Jin <longxjca@gmail.com>
2020-11-04 19:57:05 -0600

    added actions and veiws. Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'customers/index'
-  get 'customers/alphabetized'
-  get 'customers/missing_email'
+  # get 'customers/index'
+  root to: "customers#index"
+  get "customers/alphabetized", to: "customers#alphabetized", as: "alphabetized"
+  get "customers/missing_email", to: "customers#missing_email", as: "missing_email"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
